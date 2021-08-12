a772d4f6b3c931c79a5f10091fe23b5131155440
valera <lyudfaffnir@gmail.com>
2021-08-12 10:20:13 +0300

    displays table

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,46 @@
   <!DOCTYPE html>
   <html>
     <div id="app">
-      {{ this.transactions }}
+      
+      <div class='table-top'>
+        <div> Currencies </div>
+        <button>Edit table </button>
+        <input placeholder="Search" />
+      </div>
+
+      <div class='table'>
+
+        <div class='column'>
+          <p>Last update</p>
+          <div v-for='transaction in transactions' :key="transaction.key">{{ transaction.last_update_at}} </div>
+        </div>
+
+        <div class='column'>
+          <p>Code</p>
+          <div v-for='transaction in transactions' :key="transaction.key">{{ transaction.code}} </div>
+        </div>
+
+        <div class='column'>
+          <p>Name</p>
+          <div v-for='transaction in transactions' :key="transaction.key">{{ transaction.name}} </div>
+        </div>
+
+        <div class='column'>
+          <p>Deposit enabled</p>
+          <div v-for='transaction in transactions' :key="transaction.key">{{ transaction.deposit_enabled }} </div>
+        </div>
+
+        <div class='column'>
+          <p>Withdrawal enabled</p>
+          <div v-for='transaction in transactions' :key="transaction.key">{{ transaction.withdrawal_enabled}} </div>
+        </div>
+
+        <div class='column'>
+          <p>Trading enabled</p>
+          <div v-for='transaction in transactions' :key="transaction.key">{{ transaction.trading_enabled}} </div>
+        </div>
+
+      </div>
     </div>
   </html>
 </template>
@@ -17,7 +56,7 @@ export default {
   },
 
   mounted() {
-    let path = "exercise.json";
+    let path = "/exercise.json";
     let xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -34,82 +73,4 @@ export default {
 };
 </script>
 
-<style>
-#content {
-  text-align: center;
-  color: rgb(0, 0, 0);
-  background-color: rgb(249, 255, 255);
-
-  border: black 1px solid;
-
-  font-size: larger;
-  max-width: 50em;
-
-  padding-bottom: 2em;
-}
-
-.top {
-  display: grid;
-  grid-template-columns: auto auto auto;
-  padding: 0.5em 1em;
-}
-
-#top-edit-table {
-  position: relative;
-}
-
-#top-edit-table-button {
-  width: 15em;
-  border-radius: 0px;
-  border: black 1px solid;
-}
-
-#top-edit-table-button:active {
-  background-color: rgb(182, 182, 181);
-}
-
-.top-dropdown-menu {
-  display: none;
-  grid-auto-columns: auto;
-  position: absolute;
-
-  width: 12em;
-  font-size: smaller;
-
-  left: 4.5em;
-}
-
-.dropdown-element {
-  border: black 1px solid;
-  text-align: left;
-}
-
-#top-name {
-  font-weight: 600;
-}
-
-#top-search-input {
-  min-width: 80%;
-  padding-left: 2em;
-}
-
-.table {
-  display: grid;
-  grid-template-columns: repeat(6, auto);
-}
-
-.table-element {
-  font-size: 0.8em;
-  font-weight: 100;
-
-  padding: 0.3em 1em;
-}
-
-.table-element::after {
-  border: black 1px solid;
-}
-
-.table-top {
-  font-weight: 700;
-}
-</style>
+<style></style>
